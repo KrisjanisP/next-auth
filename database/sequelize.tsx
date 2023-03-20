0c6dc5754ed51201d8ff7b9ff19c38704729676a
KrisjanisP <krisjanispetrucena@gmail.com>
2023-03-20 12:15:07 +0200

    database connection

---
+++ database/sequelize.tsx
@@ -0,0 +1,9 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+const sequelize = new Sequelize("postgres://user:pass@example.com:5432/dbname");
+
+sequelize.define("User", {
+  username: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+});
